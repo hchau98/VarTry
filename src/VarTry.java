9026b84fa2bad0145122b024d61d2927e29930cb
hchau98 <chauhenry02@gmail.com>
2020-04-14 11:23:37 -0400

    First Commit

---
+++ src/VarTry.java
@@ -0,0 +1,12 @@
+public class VarTry {
+    public static void main(String[] argv) {
+        int test_score = 50;
+        double price = 24.99;
+        String username = "Henry";
+        System.out.println("Test score is " + test_score);
+        System.out.println("Price is " + price);
+        System.out.println("Username is " + username);
+
+
+    }
+}
